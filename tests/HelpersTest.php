ac5b6a8053c9a8d2fe66b9d5c55a0aa802295055
Wahyu Kristianto <w.kristories@gmail.com>
2020-01-07 23:28:41 +0700

    Add tests

---
+++ tests/HelpersTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Kristories\Helpers\Tests;
+
+use Kristories\Helpers\ServiceProvider;
+use Orchestra\Testbench\TestCase;
+
+class HelpersTest extends TestCase
+{
+    public function testExample()
+    {
+        $this->assertEquals(1, 1);
+    }
+}
