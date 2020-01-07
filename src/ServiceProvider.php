d31559cf6dfbfe1905f650d7b02945370c8d5a33
Wahyu Kristianto <w.kristories@gmail.com>
2020-01-07 23:15:28 +0700

    Add boot method

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -7,4 +7,23 @@ use Kristories\Helpers\Console\Commands\HelperMakeCommand;
 class ServiceProvider extends \Illuminate\Support\ServiceProvider
 {
     const CONFIG_PATH = __DIR__ . '/../config/helpers.php';
+
+    /**
+     * Bootstrap the application events.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->publishes([
+            self::CONFIG_PATH => config_path('helpers.php'),
+        ], 'config');
+
+        if ($this->app->runningInConsole()) {
+            $this->commands([
+                HelperMakeCommand::class,
+            ]);
+        }
+    }
+
 }
