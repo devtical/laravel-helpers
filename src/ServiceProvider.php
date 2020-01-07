488a2efc72f3c194fbadee10b40a2e886bba9179
Wahyu Kristianto <w.kristories@gmail.com>
2020-01-07 23:15:10 +0700

    Add config path

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -6,4 +6,5 @@ use Kristories\Helpers\Console\Commands\HelperMakeCommand;
 
 class ServiceProvider extends \Illuminate\Support\ServiceProvider
 {
+    const CONFIG_PATH = __DIR__ . '/../config/helpers.php';
 }
